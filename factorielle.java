cb9d85c283b899eb30287f957c492e7b121f9251
Salim Boumbar <salimbsalim2015@gmail.com>
2018-06-14 10:11:53 -0400

    j'ai ajouté du code

--- factorielle.java
+++ factorielle.java
@@ -1,12 +1,13 @@
 public class factorielle {
     public static void main(String[] args) {
         double factoriel = 1;
-        int valeur;
+        double valeur;
         valeur = Integer.parseInt(args[0]);
         while (valeur > 1) {
                factoriel = factoriel * valeur;
                valeur = valeur - 1;
         }
-        System.out.println("La factorielle est : " + factoriel);
+        System.out.println("La factorielle est : " );
+        System.out.println(factoriel);
     }
 }
